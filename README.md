8408b80cd36cbf6a4c5607c658666a2b4cb68698
Geovannarc <geovannarcf@gmail.com>
2021-11-03 18:35:27 -0300

    first commit
    first commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # memoryGame
  Rick and Morthy's themed memory game, developed with vanilla javascript.
+
+It's a pretty simple game, where you've got to choose 2 cards at a time, and find all the pairs as quick as you can. 
+
+------- 🎮 -------
+
+Jogo da memória do Rick and Morthy, desenvolvido com Vanilla JavaScript
+É um jogo bem simples, onde você deve escolher 2 cartas por vez, até encontrar todos os pares, o mais rápido que conseguir.
+
+------- 🚀 -------
+
+Functionalities I've learned:
+
+    set timeoutRunning = false; true when setTimeOut function is happening. Only flip cards when it's false --> important because users can click on another card while setTimeOut is running;
+
+    to start a new game you need to set everything to it's initial state, including the grid -> innerHTML to ""; 
+
+    using intervalHandle + clearInterval is better than using only timer.textContent = parseInt(timer.textContent) + 1
---
+++ script.js
@@ -0,0 +1,150 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cardArr = [
+        {
+            name: 'card1',
+            img: 'file:///C:/Users/geovanna.freitas/Desktop/memoryGame/imgs/card1.png'
+        },
+        {
+            name: 'card1',
+            img: 'file:///C:/Users/geovanna.freitas/Desktop/memoryGame/imgs/card1.png'
+        },
+        {
+            name: 'card2',
+            img: 'file:///C:/Users/geovanna.freitas/Desktop/memoryGame/imgs/card2.png'
+        },
+        {
+            name: 'card2',
+            img: 'file:///C:/Users/geovanna.freitas/Desktop/memoryGame/imgs/card2.png'
+        },
+        {
+            name: 'card3',
+            img: 'file:///C:/Users/geovanna.freitas/Desktop/memoryGame/imgs/card3.jpg'
+        },
+        {
+            name: 'card3',
+            img: 'file:///C:/Users/geovanna.freitas/Desktop/memoryGame/imgs/card3.jpg'
+        },
+        {
+            name: 'card4',
+            img: 'file:///C:/Users/geovanna.freitas/Desktop/memoryGame/imgs/card4.jpg'
+        },
+        {
+            name: 'card4',
+            img: 'file:///C:/Users/geovanna.freitas/Desktop/memoryGame/imgs/card4.jpg'
+        },
+        {
+            name: 'card5',
+            img: 'file:///C:/Users/geovanna.freitas/Desktop/memoryGame/imgs/card5.jpg'
+        },
+        {
+            name: 'card5',
+            img: 'file:///C:/Users/geovanna.freitas/Desktop/memoryGame/imgs/card5.jpg'
+        },
+        {
+            name: 'card6',
+            img: 'file:///C:/Users/geovanna.freitas/Desktop/memoryGame/imgs/card6.jpg'
+        },
+        {
+            name: 'card6',
+            img: 'file:///C:/Users/geovanna.freitas/Desktop/memoryGame/imgs/card6.jpg'
+        }
+    ]
+        
+    const grid = document.querySelector('.grid')
+    const resultDisplay = document.querySelector('#result');
+    let cardsChosen;
+    let cardsChosenId;
+    let cardsWon;
+    let timeoutRunning = false; //important bc users can click while setTimeOut is running --> bugou tudo
+    let currentTime;
+    let intervalHandle;
+    let timer = document.querySelector('#timer');
+
+    function startGame(){
+        grid.innerHTML = "";
+        resultDisplay.textContent =" "+ 0;
+        cardsWon = [];
+        cardsChosenId = [];
+        cardsChosen = [];
+        currentTime = 0;
+        intervalHandle = 1;
+        createBoard();
+        cardArr.sort(() => 0.5 - Math.random());
+            if(intervalHandle){
+                clearInterval(intervalHandle);
+                intervalHandle = setInterval(function(){
+                    currentTime++;
+                    timer.textContent = currentTime;
+                }, 1000);
+            }
+    } 
+    startGame();
+
+    function createBoard(){
+        for(let i=0; i< cardArr.length; i++){
+            let card = document.createElement('img');
+            card.className = 'card'; //important if there's going to be any other images that are not cards --> don't use 'card' w/ bootstrap --> bugou tudo
+            card.setAttribute('src', 'file:///C:/Users/geovanna.freitas/Desktop/memoryGame/imgs/verso.png');
+            card.setAttribute('data-id', i);
+            grid.appendChild(card);
+            card.addEventListener('click', flipCard);
+        }
+    }
+
+    function checkForMatch(){
+        timeoutRunning = false;
+        let cards = document.querySelectorAll('img.card');
+        let optionOneId = cardsChosenId[0];
+        let optionTwoId = cardsChosenId[1];
+            if(cardsChosen[0] === cardsChosen[1]){
+                cards[optionOneId].setAttribute('src', 'file:///C:/Users/geovanna.freitas/Desktop/memoryGame/imgs/white.png');
+                cards[optionTwoId].setAttribute('src', 'file:///C:/Users/geovanna.freitas/Desktop/memoryGame/imgs/white.png');
+                cardsWon.push(cardsChosen[0]);
+                cardsWon.push(cardsChosen[1]);
+            }else{
+                cards[optionOneId].setAttribute('src', 'file:///C:/Users/geovanna.freitas/Desktop/memoryGame/imgs/verso.png');
+                cards[optionTwoId].setAttribute('src', 'file:///C:/Users/geovanna.freitas/Desktop/memoryGame/imgs/verso.png');
+            }
+            cardsChosen = [];
+            cardsChosenId = [];
+            resultDisplay.textContent = " " +Math.floor(cardsWon.length * 1000 * 1/parseInt(timer.textContent));
+            if(cardsWon.length === cardArr.length){
+
+                Swal.fire({
+                    width: 1000,
+                    title: 'PARABÉNS',
+                    html: `Você fez ${resultDisplay.textContent} pontos!`,
+                    padding: '1em',
+                    background: 'whitesmoke',
+                    showCloseButton: true,
+                    confirmButtonText: 'JOGAR DE NOVO',
+                    confirmButtonColor: 'purple',
+                    showCancelButton: false,
+                    backdrop: `
+                        rgba(0,0,123,0.1)
+                    `
+                }).then((result) => {
+                        if(result.isConfirmed){
+                            startGame();
+                        }
+                    })
+            }
+    }
+
+    function flipCard(){
+            if(timeoutRunning){
+                return;
+            }
+            let cardId = this.getAttribute('data-id');
+            if(cardsChosenId.length && cardsChosenId[0] == cardId){
+                return;
+            }
+            cardsChosen.push(cardArr[cardId].name);
+            cardsChosenId.push(cardId);
+            this.setAttribute('src', cardArr[cardId].img);
+                if(cardsChosen.length === 2){
+                    timeoutRunning = true;
+                    setTimeout(checkForMatch, 350)
+                }
+        }
+})
